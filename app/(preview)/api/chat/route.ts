83ec2c57d0d4ee3ae31c0f77885018ffb0007f78
Aadil-Hasun <aadilhasun@gmail.com>
2025-04-24 23:02:15 +0530

    Remove Identity & IdentityType

--- app/(preview)/api/chat/route.ts
+++ app/(preview)/api/chat/route.ts
@@ -11,12 +11,13 @@ console.log("Creating Pica with server URL:", "https://platform-backend.inhotel.
 const pica = new Pica("sk_test_1_3pejYG_SdSxV9xkt5_GA8WoMsSnfBHvY1qpGhlX-6DKd9kyZO3ee9hWfjGWpt5dY0AzxvM51q6_45_Q6bJTWCTuax7yq4X96nhvB0uTwhhLlsxyJm02JqasmdeDVeHt08GxGPoiBc7I9u00-1EKOejw62kNO0M1EaEFqwaGXw1Y8IfFH", {
   connectors: ["*"],
   serverUrl: "https://platform-backend.inhotel.io",
+  authkit: true,
   knowledgeAgent: true,
   knowledgeAgentConfig: {
     includeEnvironmentVariables: false,
   },
-  identity: "65648fa26b1eb500122c5323", // a meaningful identifier (i.e., userId, teamId or organizationId)
-  identityType: "user"
+  // identity: "65648fa26b1eb500122c5323", // a meaningful identifier (i.e., userId, teamId or organizationId)
+  // identityType: "user"
 });
 
 // Use this if you need to inspect the pica object
