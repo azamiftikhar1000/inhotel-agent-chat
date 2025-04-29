2c46ccd305199bbaa4c9bb56f6a144e553131cf2
Aadil-Hasun <aadilhasun@gmail.com>
2025-04-29 11:48:02 +0530

    use google

--- app/(preview)/api/chat/route.ts
+++ app/(preview)/api/chat/route.ts
@@ -26,9 +26,9 @@ console.log("Pica object:", JSON.stringify(pica, null, 2));
   const system = await pica.generateSystemPrompt();
 
   const stream = streamText({
-    // model: google("gemini-2.5-flash-preview-04-17"),
+    model: google("gemini-2.5-flash-preview-04-17"),
     // model: openai("gpt-4.1"),
-    model: anthropic("claude-3-7-sonnet-20250219"),
+    // model: anthropic("claude-3-7-sonnet-20250219"),
     system,
     tools: {
       ...pica.oneTool,
